bb7ca6d0c799abb5cf56ee1a1855241804c73d98
THIAGO-BARBOSA-ARAUJO <thiagobarbosa.araujo@outlook.com>
2022-08-29 15:15:35 -0300

    bonus feitos

--- app.js
+++ app.js
@@ -33,28 +33,158 @@ const arrTweets = [
         username: "lula",
 	    tweet: "eu vou ser seu presidente esse ano!"
     },
+    {
+        username: "bobesponja",
+	    tweet: "eu sou o naruto esponja"
+    },
+    {
+        username: "patrick",
+	    tweet: "eu sou patrick do sharingan"
+    },
+    {
+        username: "lula",
+	    tweet: "eu vou ser seu presidente esse ano!"
+    },
+    {
+        username: "bobesponja",
+	    tweet: "eu sou o naruto esponja"
+    },
+    {
+        username: "patrick",
+	    tweet: "eu sou patrick do sharingan"
+    },
+    {
+        username: "lula",
+	    tweet: "eu vou ser seu presidente esse ano!"
+    },
+    {
+        username: "bobesponja",
+	    tweet: "eu sou o naruto esponja"
+    },
+    {
+        username: "patrick",
+	    tweet: "começo da segunda parte"
+    },
+    {
+        username: "lula",
+	    tweet: "eu vou ser seu presidente esse ano!"
+    },
+    {
+        username: "bobesponja",
+	    tweet: "eu sou o naruto esponja"
+    },
+    {
+        username: "patrick",
+	    tweet: "eu sou patrick do sharingan"
+    },
+    {
+        username: "lula",
+	    tweet: "eu vou ser seu presidente esse ano!"
+    },
+    {
+        username: "bobesponja",
+	    tweet: "eu sou o naruto esponja"
+    },
+    {
+        username: "patrick",
+	    tweet: "eu sou patrick do sharingan"
+    },
+    {
+        username: "lula",
+	    tweet: "eu vou ser seu presidente esse ano!"
+    },
+    {
+        username: "bobesponja",
+	    tweet: "eu sou o naruto esponja"
+    },
+    {
+        username: "patrick",
+	    tweet: "eu sou patrick do sharingan"
+    },
+    {
+        username: "lula",
+	    tweet: "começo da terceira parte"
+    }
 ]
-
+    console.log()
 app.post("/sign-up", (req, res)=>{
-    arrUsers.push(req.body)
-    res.send()
+    const { avatar } = req.body
+    const { user : username } = req.headers
+
+    if(!username || !avatar ){
+        res.status(400).send("Todos os campos são obrigatórios")
+        return
+    }
+    arrUsers.push({username, avatar})
+    res.status(201).send("OK")
 })
 
 app.get("/tweets", (req, resp)=>{
+    
+    const { page } = req.query
 
-    const novoarrTweets = arrTweets.map((tweet)=>{
+    const pagina = Number(page)
+    
+    const limitpaginas = Math.ceil(arrTweets.length / 10)
+
+    if(page){
+        if(pagina >= 1 && pagina <= limitpaginas){
+            const novoarrTweets = arrTweets.map((tweet)=>{
+                return {
+                    ...tweet,
+                    avatar: arrUsers.find((user)=>user.username === tweet.username).avatar 
+                }
+            })
+            resp.send(novoarrTweets.slice(pagina*10-10, pagina*10))
+        }else{
+            resp.status(400).send("Informe uma página válida!")
+            return
+        }
+    }
+    else if(page === ""){
+        resp.status(400).send("Informe uma página!")
+        return
+    }
+    else{
+        const novoarrTweets = arrTweets.map((tweet)=>{
+            return {
+                ...tweet,
+                avatar: arrUsers.find((user)=>user.username === tweet.username).avatar 
+            }
+        })
+        resp.send(novoarrTweets.slice(0, 10))
+    }
+})
+
+app.get("/tweets/:username", (req, res)=>{
+    const { username } = req.params
+
+    const userExists = arrUsers.find(exists => exists.username === username)
+
+    if(!userExists) {
+        res.status(404).send("Usuário não encontrado")
+        return
+    }
+
+    const novoarrTweets = arrTweets.filter(tweet => tweet.username === username).map((tweet)=>{
         return {
             ...tweet,
             avatar: arrUsers.find((user)=>user.username === tweet.username).avatar 
         }
     })
-    resp.send(novoarrTweets.slice(0, 10))
+    res.send(novoarrTweets.slice(0, 10))
 })
 
 app.post("/tweets", (req, resp)=>{
-    arrTweets.unshift(req.body)
-    resp.send("ok")
+    const { username, tweet } = req.body
+    if(!username || !tweet ){
+        resp.status(400).send("Todos os campos são obrigatórios")
+        return
+    }
+    arrTweets.unshift({username, tweet})
+    resp.status(201).send("ok")
 })
+
 
 app.listen(5000,()=>{
     console.log("ta funfando, que beleuza!")
